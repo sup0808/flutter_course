7f07dd0e0ca843019749f23143872427b5ea7e6e
Supriya Gupta <supriya.gupta@infogin.com>
2020-06-02 16:04:26 +0530

    First commit flutter Learning

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var questions=['What \' is our jjjd',
+                    'What \'s your lunch'];
+    // TODO: implement build
+    return MaterialApp(
+      home:Scaffold(
+        appBar:AppBar(
+          title:Text('My First App'),
+       ),
+        body: Column(
+          children: [
+            Text('This Questions!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,)
+          ],
+        ),
+      ),
+    );
+  }
+}
